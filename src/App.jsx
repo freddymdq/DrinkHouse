8dee9eac379434d157ee77e4af77864c19eba420
Fede Mosquera <freddymdq@gmail.com>
2022-11-20 12:09:20 -0300

    Nuevo Repositiorio

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Fede <code>Mosquera</code> genio.
         </p>
         <a
           className="App-link"
